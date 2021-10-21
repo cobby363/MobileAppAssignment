dced205f018522d18cd4b6ef43bbd036c0e8a2ba
Cathal <47002668+cobby363@users.noreply.github.com>
2021-10-21 15:02:55 +0100

    Removing project to create a new one as it broke before
    My configurations were completely messed up and it failed to recognise many aspects of my assignment that were working outside github. Going to now try to create a new project inside of this repository and fingers crossed it will work this time

--- MobileAppAssignment/src/main/kotlin/org/wit/VideoManagement/console/models/VideoModel.kt
+++
@@ -1,8 +0,0 @@
-package org.wit.videoManagement.console.models
-
-data class VideoModel(var id: Long = 0,
-                      var channel: String = "",
-                      var video: String = "",
-                      var likedAbout: String = "",
-                      var starRating: Float = 0.0f,
-                      var tags: String = "")
--- MobileAppAssignment/src/main/kotlin/org/wit/VideoManagement/console/models/VideoStore.kt
+++
@@ -1,9 +0,0 @@
-package org.wit.videoManagement.console.models
-
-interface VideoStore {
-
-    fun findAll(): List<VideoModel>
-    fun findOne(id: Long): VideoModel?
-    fun create(video: VideoModel)
-    fun update(video: VideoModel)
-}
