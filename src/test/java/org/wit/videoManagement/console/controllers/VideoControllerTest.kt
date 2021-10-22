b2d94d28afe183053c2de6698f2eb501afad7185
Cathal <47002668+cobby363@users.noreply.github.com>
2021-10-22 21:57:12 +0100

    Create VideoControllerTest.kt

---
+++ src/test/java/org/wit/videoManagement/console/controllers/VideoControllerTest.kt
@@ -0,0 +1,36 @@
+package org.wit.videoManagement.console.controllers
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class VideoControllerTest {
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun list() {
+    }
+
+    @Test
+    fun searchByTags() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun search() {
+    }
+
+    @Test
+    fun testSearch() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
